93379a269314eba5d703191e4503fd86f340132d
王雷 <wanglei@chinacache.com>
2013-08-29 18:11:46 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zaoke::Application.config.secret_token = 'a2771744a696936072175fe8ab6c0c85ef58a316c49bd49f3ee7fe873c4e6665a93654f99cc4d0edab34d21359041dd6a7501c46fff97a2b8ef479a8ff747f33'
